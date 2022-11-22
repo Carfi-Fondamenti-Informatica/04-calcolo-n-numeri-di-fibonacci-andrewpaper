2dad614bb1d98726ae09f3995634e99b02caff5b
andrewpaper <118001300+andrewpaper@users.noreply.github.com>
2022-11-22 21:09:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
+   
+   int a=1;
+   int b=1;
+   int risultato=0;
+   int n=0; cin >> n;
+   if (n<2) {
+      cout << "errore";
+   }else{
+      cout << "1" << endl;
+      cout << "1" << endl;
+   for (int i=0; i<= (n-3); i++) {
+      risultato=a+b;
+      cout << risultato << endl;
+      a=b;
+      b=risultato;
+   }
+   }
+  
    return 0;
 }
